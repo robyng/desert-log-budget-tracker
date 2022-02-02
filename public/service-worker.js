1dd8ef7a60cec77ddd40414da7d2b1618eb013f8
robyng <robyngraham01@gmail.com>
2022-02-02 10:55:39 -0800

    fix icon names and change css and js paths

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,14 +4,17 @@ const DATA_CACHE_NAME = 'data-cache-v2';
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  '/favicon.ico',
   '/manifest.json',
-  '/app.js',
-  '/assets/css/style.css',
-  '/assets/images/icons/icon-72x72.png',
-  '/assets/images/icons/icon-96x96.png',
-  '/assets/images/icons/icon-128x128.png',
-  '/assets/images/icons/icon-144x144.png'
+  '/js/index.js',
+  '/css/styles.css',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
 ];
 
 // Install the service worker
