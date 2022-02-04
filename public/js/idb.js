681b70b3a0dc5ea551c2ecf50c11551cb2ddc850
robyng <robyngraham01@gmail.com>
2022-02-04 00:15:54 -0800

    added upload budget function, event listener for online

--- public/js/idb.js
+++ public/js/idb.js
@@ -38,3 +38,51 @@ function saveRecord(record) {
     // add record to your store with add method
     budgetObjectStore.add(record);
   }
+
+
+  function uploadBudget() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+  
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('new_budget');
+  
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+  
+
+  // upon a successful .getAll() execution, run this function
+getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_budget'], 'readwrite');
+          // access the new_budget object store
+          const budgetObjectStore = transaction.objectStore('new_budget');
+          // clear all items in your store
+          budgetObjectStore.clear();
+
+          alert('All offline entries submitted');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+  // listen for app coming back online
+window.addEventListener('online', uploadBudget);
